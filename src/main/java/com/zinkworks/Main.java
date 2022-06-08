75a7c4e93285a3b67a4067d78b949e6df989371c
Ruth Cassidy <ruth.cassidy@zinkworks.com>
2022-06-08 11:28:18 +0100

    Add composition example

--- src/main/java/com/zinkworks/Main.java
+++ src/main/java/com/zinkworks/Main.java
@@ -1,7 +1,17 @@
-package main.java.com.zinkworks;
+package com.zinkworks;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dimentions dimentions = new Dimentions(20, 20, 5);
+        Case theCase = new Case("2208", "Dell", "240", dimentions);
+
+        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
+
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+        thePC.getMonitor().drawPixelAt(1500, 1200, "red" );
+        thePC.getMotherboard().loadProgram("Windows 1.0");
+        thePC.getTheCase().pressPowerButton();
     }
 }
